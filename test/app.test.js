a43216e674fa34d214de9f6fa31f3005deadd72a
MichelQuivront <michelquivront1@gmail.com>
2026-09-10 14:08:54 +0200

    Update test

--- test/app.test.js
+++ test/app.test.js
@@ -3,3 +3,4 @@ const assert=require("node:assert/strict");
 test("root contains service name",()=>assert.equal("platform-demo","platform-demo"));
 test("health is healthy",()=>assert.equal("ok","ok"));
 test("GET /version returns version", async () => {});
+bklablalablbal
